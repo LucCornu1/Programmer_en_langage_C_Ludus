68fd7c0c881591d5a9b259b33d56d95e0f249675
CornuLuc <tempusfugit.games@gmail.com>
2019-10-21 11:29:25 +0200

    Cours type C

--- Prog_en_C_LC_V1-0/main.c
+++ Prog_en_C_LC_V1-0/main.c
@@ -9,7 +9,7 @@
 // Exemple de proramme en C
 
 // MAIN
-int main()
+/*int main()
 {
     int i;
     float x;
@@ -30,4 +30,24 @@ int main()
     printf("Travail termine");
 
     return 0;
+}*/
+
+void main(){
+    int i;
+    float x;
+    float racx;
+
+    printf("Bonjour\n"); //méthode de sortie standard qui renvoie sur la console, un flux de données
+    printf("Calcul de %d racines carrees \n",NFOIS); //%d indique le format de la variable qui va être affichée (ici int)
+    for (i=0; i<NFOIS; i++){
+        printf("Donnez un nombre : ");
+        scanf("%f",&x); //fonction servant à récupérer la saisie de donnée d'un certain type. %f indique le format qui va être affichée (ici float)
+        if (x<0.0) //.0 !!! important car c'est un réel !!!
+            printf("Le nombre %f ne possede pas de racine carre\n",x);
+        else{
+            racx=sqrt(x);
+            printf("Le nombre %f a pour racine carree : %f\n",x,racx);
+        }
+    }
+    printf("Travail termine");
 }
